a1181c76b1f7a275e2743377bcd6f76a58a9417e
petruki <31597636+petruki@users.noreply.github.com>
2020-08-10 21:58:07 -0700

    fix: Removed hash. Redirect coming from oauth services wasnt working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, SecurityContext } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
