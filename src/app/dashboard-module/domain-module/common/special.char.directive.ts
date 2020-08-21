288b84fcfaddfdccefed304d3884987a4c7071ea
petruki <31597636+petruki@users.noreply.github.com>
2020-08-21 12:27:36 -0700

    feat: Added input filter directives

---
+++ src/app/dashboard-module/domain-module/common/special.char.directive.ts
@@ -0,0 +1,48 @@
+import { Directive, Input, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+    selector: '[special-chars]'
+})
+export class SpecialCharacterDirective {
+
+    @Input() allowRegexStr: string = '^[a-zA-Z0-9_]*$';
+    @Input() autoUpper: boolean = false;
+    @Input() autoLower: boolean = false;
+    @Input() autoUnderscore: boolean = false;
+
+    constructor(private el: ElementRef) { }
+
+    @HostListener('input', ['$event']) onInput(event) {
+        if (this.autoUpper)
+            event.target.value = event.target.value.toUpperCase();
+        if (this.autoLower)
+            event.target.value = event.target.value.toLowerCase();
+        if (this.autoUnderscore)
+            event.target.value = event.target.value.replace(/\s/g, '_');
+    }
+
+    @HostListener('keypress', ['$event']) onKeyPress(event) {
+        if (this.autoUnderscore && event.key === ' ')
+            return true;
+        return new RegExp(this.allowRegexStr).test(event.key);
+    }
+
+    @HostListener('paste', ['$event']) blockPaste(event: KeyboardEvent) {
+        this.validateFields(event);
+    }
+
+    validateFields(event) {
+        setTimeout(() => {
+            if (this.autoUnderscore)
+                this.el.nativeElement.value = this.el.nativeElement.value.replace(/\s/g, '_');
+            if (this.autoUpper)
+                this.el.nativeElement.value = this.el.nativeElement.value.toUpperCase();
+            if (this.autoLower)
+                this.el.nativeElement.value = this.el.nativeElement.value.toLowerCase();
+
+            this.el.nativeElement.value =  this.el.nativeElement.value.replace(this.allowRegexStr, '');
+            event.preventDefault();
+        }, 100);
+    }
+
+}
