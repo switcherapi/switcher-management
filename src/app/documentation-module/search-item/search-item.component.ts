cb2dfe164ef72d37bdb36e885669ce812a24b50b
petruki <31597636+petruki@users.noreply.github.com>
2020-09-05 19:54:14 -0700

    feat: Improved documentation - changed prism style

--- src/app/documentation-module/search-item/search-item.component.ts
+++ src/app/documentation-module/search-item/search-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { SkimmingResult } from '../model/skimming-response';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-search-item',
@@ -26,6 +27,13 @@ export class SearchItemComponent {
     }
     const route = file.replace('.md', '');
     this.router.navigate(['/documentation/' + route]);
+  }
+
+  renderSegment(data: string, documentToRender: string) {
+    if (data)
+        return data.replace(/\[\$ASSETS_LOCATION\]/g, environment.docsUrl);
+      else
+        return `${environment.docsUrl}${documentToRender} not found`;
   }
 
 }
