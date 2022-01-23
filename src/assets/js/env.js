2651d535aca8544434949ff1ded224ab3c4a8e94
petruki <31597636+petruki@users.noreply.github.com>
2022-01-23 11:55:15 -0800

    Create exposed settings for the container environment

--- src/assets/js/env.js
+++ src/assets/js/env.js
@@ -2,4 +2,8 @@
     window["env"] = window["env"] || {};
     window["env"]["SWITCHERAPI_URL"] = "http://localhost:3000";
     window["env"]["SM_IP"] = "localhost";
+    window["env"]["GOOGLE_RECAPTCHA"] = undefined;
+    window["env"]["GITHUB_CLIENTID"] = undefined;
+    window["env"]["BITBUCKET_CLIENTID"] = undefined;
+    window["env"]["SWITCHERSLACKAPP_URL"] = undefined;
   })(this);
