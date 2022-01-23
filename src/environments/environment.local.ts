ae6145d0705e94e855c6994afd4960cbf06e69ac
petruki <31597636+petruki@users.noreply.github.com>
2022-01-22 21:23:21 -0800

    Closes #210

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,14 +1,13 @@
 export const environment = {
   production: true,
-  docker: true,
-  recaptchaPublicKey: undefined,
-  githubApiClientId: undefined,
-  bitbucketApiClientId: undefined,
+  recaptchaPublicKey: window["env"]["GOOGLE_RECAPTCHA"] || undefined,
+  githubApiClientId: window["env"]["GITHUB_CLIENTID"] || undefined,
+  bitbucketApiClientId: window["env"]["BITBUCKET_CLIENTID"] || undefined,
   teamInviteLink: `http://${window["env"]["SM_IP"] || 'localhost'}/collab/join`,
   domainTransferLink: `http://${window["env"]["SM_IP"] || 'localhost'}/domain/transfer`,
   apiUrl: window["env"]["SWITCHERAPI_URL"] || "http://localhost:3000",
   skimmingApi: 'https://dance-0m7.begin.app',
-  slackUrl: undefined,
+  slackUrl: window["env"]["SWITCHERSLACKAPP_URL"] || undefined,
   docsUrl: 'assets/',
   timeout: 5000
 };
