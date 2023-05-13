df89df7723c5a0acb4c18349979f4119a96437ee
Roger Floriano <31597636+petruki@users.noreply.github.com>
2023-05-12 20:49:38 -0700

    #307 - Added Relay verification services (#309)

--- src/app/model/config.ts
+++ src/app/model/config.ts
@@ -19,10 +19,15 @@ export class ConfigRelay {
   method: string;
   auth_prefix: string = '';
   auth_token: Map<string, string> = new Map<string, string>();
-  verified: boolean = false;
+  verified: Map<string, boolean> = new Map<string, boolean>();
   verification_code: string;
 }
 
 export class ConfigRelayStatus {
   activated: Map<string, boolean> = new Map<string, boolean>();
+}
+
+export class ConfigRelayVerification {
+  code: string;
+  status: string;
 }
