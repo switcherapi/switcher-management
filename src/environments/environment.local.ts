7e50b5ecca6b8106490358e601c1290c31e22e95
petruki <31597636+petruki@users.noreply.github.com>
2024-03-09 15:42:23 -0800

    Fixes loading environment default properties

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,20 +1,33 @@
 import { environment_global } from "./environment.global";
 
+const getEnv = (key: string, defaultValue?: string | boolean) => {
+  const value = window["env"][key];
+  if (value === undefined || value === "") {
+    return defaultValue;
+  }
+
+  if (typeof defaultValue === "boolean") {
+    return value === "true";
+  }
+
+  return value;
+}
+
 export const environment = {
   production: true,
   version: environment_global.version,
-  releaseTime: window["env"]["RELEASE_TIME"] || undefined,
-  recaptchaPublicKey: window["env"]["GOOGLE_RECAPTCHA"] || undefined,
-  allowInternalAuth: window["env"]["ALLOW_INTERNAL_AUTH"] || true,
-  allowHomeView: window["env"]["ALLOW_HOME_VIEW"] || false,
-  githubApiClientId: window["env"]["GITHUB_CLIENTID"] || undefined,
-  bitbucketApiClientId: window["env"]["BITBUCKET_CLIENTID"] || undefined,
-  teamInviteLink: `${window["env"]["SM_IP"] || 'localhost'}/collab/join`,
-  domainTransferLink: `${window["env"]["SM_IP"] || 'localhost'}/domain/transfer`,
-  apiUrl: window["env"]["SWITCHERAPI_URL"] || "http://localhost:3000",
+  releaseTime: getEnv("RELEASE_TIME"),
+  recaptchaPublicKey: getEnv("GOOGLE_RECAPTCHA"),
+  allowInternalAuth: getEnv("ALLOW_INTERNAL_AUTH", environment_global.allowInternalAuth),
+  allowHomeView: getEnv("ALLOW_HOME_VIEW", environment_global.allowHomeView),
+  githubApiClientId: getEnv("GITHUB_CLIENTID"),
+  bitbucketApiClientId: getEnv("BITBUCKET_CLIENTID"),
+  teamInviteLink: `${getEnv('SM_IP', 'localhost')}/collab/join`,
+  domainTransferLink: `${getEnv('SM_IP', 'localhost')}/domain/transfer`,
+  apiUrl: getEnv('SWITCHERAPI_URL', 'http://localhost:3000'),
   apiSearchDocsUrl: environment_global.apiSearchDocsUrl,
   apiFeatureUrl: environment_global.apiFeatureUrl,
-  slackUrl: window["env"]["SWITCHERSLACKAPP_URL"] || undefined,
+  slackUrl: getEnv('SWITCHERSLACKAPP_URL'),
   docsUrl: 'assets/',
   timeout: 5000
 };
