b828e908b2736660aa600116c710acd858fc434f
petruki <31597636+petruki@users.noreply.github.com>
2024-06-05 20:22:53 -0700

    Fixed style issues introduced through Angular upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import '@angular/localize/init';
+
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
