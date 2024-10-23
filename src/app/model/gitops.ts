1dfd53b0a61a1b44b65b7af9fec25651dcf1de18
Roger Floriano <31597636+petruki@users.noreply.github.com>
2024-10-22 21:42:47 -0700

    GitOps - added un/subscribe and update tokens events (#412)

--- src/app/model/gitops.ts
+++ src/app/model/gitops.ts
@@ -1,52 +1,57 @@
 export const TOKEN_VALUE = '********************************';
 
 export class GitOpsAccount {
-    ID?: string;
-    repository: string;
-    branch?: string;
-    path?: string;
-    token?: string;
-    environment: string;
-    domain: GitOpsDomain;
-    settings: GitOpsSettings;
+  ID?: string;
+  repository: string;
+  branch?: string;
+  path?: string;
+  token?: string;
+  environment: string;
+  domain: GitOpsDomain;
+  settings: GitOpsSettings;
 }
 
 export class GitOpsDomain {
-    id: string;
-    name: string;
-    version: number;
-    lastcommit: string;
-    lastdate: string;
-    status: string;
-    message: string;
+  id: string;
+  name: string;
+  version: number;
+  lastcommit: string;
+  lastdate: string;
+  status: string;
+  message: string;
 }
 
 export class GitOpsSettings {
-    active: boolean;
-    window: string;
-    forceprune: boolean;
+  active: boolean;
+  window: string;
+  forceprune: boolean;
+}
+
+export class GitOpsAccountTokenResponse {
+  result: boolean;
+  message: string;
 }
 
 export function buildNewGitOpsAccount(environment: string, domainId: string, domainName: string): GitOpsAccount {
-    return {
-      environment,
-      repository: '',
-      branch: '',
-      path: '',
-      token: '',
-      domain: {
-        id: domainId,
-        name: domainName,
-        version: 0,
-        lastcommit: 'refresh',
-        lastdate: '',
-        status: 'Pending',
-        message: 'Creating GitOps Account'
-      },
-      settings: {
-        active: true,
-        window: '1m',
-        forceprune: false
-      }
-    };
-  }
+  return {
+    environment,
+    repository: '',
+    branch: '',
+    path: '',
+    token: '',
+    domain: {
+      id: domainId,
+      name: domainName,
+      version: 0,
+      lastcommit: 'refresh',
+      lastdate: '',
+      status: 'Pending',
+      message: 'Creating GitOps Account'
+    },
+    settings: {
+      active: true,
+      window: '1m',
+      forceprune: false
+    }
+  };
+}
