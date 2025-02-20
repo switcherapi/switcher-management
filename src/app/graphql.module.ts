74175ba20f6e6c57dbeb6d7ca7aa6bfbe90c80aa
Roger Floriano <31597636+petruki@users.noreply.github.com>
2025-02-19 18:35:21 -0800

    Bump Angular@19.1.7, upgraded apollo-angular to 10.0.1 (removed patch) (#421)

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,27 +1,23 @@
-import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
+import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client/core';
+import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { environment } from 'src/environments/environment';
-import { NgModule } from '@angular/core';
+import { inject, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 const uri = `${environment.apiUrl}/adm-graphql`;
 
+export function createApollo(): ApolloClientOptions<any> {
+  const httpLink = inject(HttpLink);
+
+  return {
+    link: httpLink.create({ uri }),
+    cache: new InMemoryCache(),
+  };
+}
+
 @NgModule({
-  imports: [BrowserModule, ApolloModule],
-  providers: [
-    {
-      provide: APOLLO_OPTIONS,
-      useFactory: (httpLink: HttpLink) => {
-        return {
-          cache: new InMemoryCache(),
-          link: httpLink.create({
-            uri,
-          }),
-        };
-      },
-      deps: [HttpLink],
-    },
-  ],
+  imports: [BrowserModule],
+  providers: [provideApollo(createApollo)]
 })
 export class GraphQLModule { }
