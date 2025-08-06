a18b0b48def82a445962af6c4efca577afe03124
Roger Floriano <31597636+petruki@users.noreply.github.com>
2025-08-05 20:38:39 -0700

    Upgraded Node to v24 (#430)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
+import '@angular/compiler'; // Required for JIT compilation
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -8,12 +9,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
-
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+platformBrowser().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
